d23eaa28db238fb77190d2e314f1e0adfff2c41c
RomanParaniuk <33425011+RomanParaniuk@users.noreply.github.com>
2020-03-28 09:13:18 +0200

    Updated horizontal moving
    +Added run and walk animations

---
+++ First One/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Joystick joystick;
+    Rigidbody2D rb;
+    Animator animator;
+
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 7f;
+    public Vector2 direction;
+    private bool facingRight = true;
+
+    [Header("Physics")]
+    public float maxSpeed = 7f;
+    public float minSpeed = 0.05f;
+    public float linearDrag = 4f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+       
+    }
+
+    void Update()
+    {
+        direction = new Vector2(joystick.Horizontal, joystick.Vertical);
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(direction.x);
+        modifyPhysics();
+    }
+
+    void moveCharacter(float horizontal)
+    {
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+
+        if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight))
+        {
+            Flip();
+        }
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+        {
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+        if (Mathf.Abs(rb.velocity.x) < minSpeed)
+        {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
+    }
+
+    void modifyPhysics()
+    {
+        bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+
+        if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+        {
+            rb.drag = linearDrag;
+        }
+        else
+        {
+            rb.drag = 0f;
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+}
