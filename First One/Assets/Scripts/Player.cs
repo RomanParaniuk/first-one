626247a7b04fac25c13b8576c804bfeaebc920a9
RomanParaniuk <33425011+RomanParaniuk@users.noreply.github.com>
2020-03-28 12:47:11 +0200

    Added player jump

--- First One/Assets/Scripts/Player.cs
+++ First One/Assets/Scripts/Player.cs
@@ -14,26 +14,60 @@ public class Player : MonoBehaviour
     public Vector2 direction;
     private bool facingRight = true;
 
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 1f;
+    public float jumpDelay = 0.25f;
+    private float jumpTimer;
+
+    [Header("Components")]
+    public LayerMask groundLayer;
+
+
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float minSpeed = 0.02f;
     public float linearDrag = 4f;
+    public float gravity = 1f;
+    public float fallMultiplier = 5f;
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.45f;
+    public Vector3 colliderOffset;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-       
+
     }
 
     void Update()
     {
-        direction = new Vector2(joystick.Horizontal, joystick.Vertical);
+        bool wasOnGround = onGround;
+        onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+
+        if (!wasOnGround && onGround)
+        {
+            StartCoroutine(JumpSqueeze(1.25f, 0.8f, 0.05f));
+        }
+
+        if (joystick.Vertical > 0.4f)
+        {
+            jumpTimer = Time.time + jumpDelay;
+        }
+        animator.SetBool("onGround", onGround);
+        direction = new Vector2(joystick.Horizontal, maxSpeed);
     }
 
     void FixedUpdate()
     {
+
         moveCharacter(direction.x);
+        if (jumpTimer > Time.time && onGround)
+        {
+            Jump();
+        }
         modifyPhysics();
     }
 
@@ -54,26 +88,79 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(0f, rb.velocity.y);
         }
         animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
-        print("Velosity is " + rb.velocity);
-        print("Horizontal is " + horizontal);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        jumpTimer = 0;
+        StartCoroutine(JumpSqueeze(0.5f, 1.2f, 0.1f));
+        animator.SetFloat("vertical", rb.velocity.y);
+
     }
 
     void modifyPhysics()
     {
         bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
 
-        if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+        if (onGround)
         {
-            rb.drag = linearDrag;
+
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
         }
         else
         {
-            rb.drag = 0f;
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if (rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            }
+            else if (rb.velocity.y > 0 && joystick.Vertical <= 0.4f)
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
         }
     }
     void Flip()
     {
         facingRight = !facingRight;
         transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+
+    IEnumerator JumpSqueeze(float xSqueeze, float ySqueeze, float seconds)
+    {
+        Vector3 originalSize = Vector3.one;
+        Vector3 newSize = new Vector3(xSqueeze, ySqueeze, originalSize.z);
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            gameObject.transform.localScale = Vector3.Lerp(originalSize, newSize, t);
+            yield return null;
+        }
+
+        t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            gameObject.transform.localScale = Vector3.Lerp(newSize, originalSize, t);
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position - colliderOffset, transform.position - colliderOffset + Vector3.down * groundLength);
     }
 }
