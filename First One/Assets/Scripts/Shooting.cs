238d6587d411b678f1d58757d6508c91ea9c0bc8
RomanParaniuk <33425011+RomanParaniuk@users.noreply.github.com>
2020-11-08 21:56:17 +0200

    After long break
    Added new Player object: now animation works with player bones
    added shooting-gun system
    player could run and fire on the same time!

--- First One/Assets/Scripts/Shooting.cs
+++ First One/Assets/Scripts/Shooting.cs
@@ -1,40 +1,113 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    Animator animator;
+
+
     [Header("Gun Details")]
-    public Joystick fireJoystick;
-    public GameObject laserPrefab;
-    public float shootDelay = 0.25f;
+    [SerializeField] Joystick fireJoystick;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] GameObject gun;
+    [SerializeField] float turnSpeed = 10f;
+    [SerializeField] float shootingSpeed = 1f;
     public float laserSpeed = 5f;
-    private float shootTimer;
+
+    [Header("Player Details")]
+    [SerializeField] GameObject playerBody;
+    public float returnToDefPosTime = 3f;
+
+
+
+    private float returnToDefPosTimer;
+    private float laserAngle;
+    private float bodyAngle;
+    private bool isShooting;
+    private Vector3 flipScale = new Vector3(-1f,1f,1f);
+    private Vector3 regularScale = new Vector3(1f, 1f, 1f);
+
 
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        isShooting = false;
+        animator = GetComponentInParent<Animator>();
+        animator.SetFloat("shootingSpeed", shootingSpeed);
+
     }
 
     void Update()
     {
-        Shoot();
+        StopAllCoroutines();
+        isShooting = GetIsShooting();
+        animator.SetBool("isShooting", isShooting);
+        if (isShooting)
+        {
+            Shoot();
+        }
+
+        if (!isShooting && returnToDefPosTimer < Time.time)
+        {
+            ReturnDefaultPosition();
+        }
+
     }
 
     public void Shoot()
     {
-        if (fireJoystick.Direction != Vector2.zero && shootTimer < Time.time)
-        {
-            float angle = Mathf.Atan2(fireJoystick.Vertical, fireJoystick.Horizontal) * Mathf.Rad2Deg;
-            GameObject laser = Instantiate(
-                   laserPrefab,
-                   transform.position,
-                   Quaternion.Euler(new Vector3(0, 0, angle))) as GameObject;
+        animator.SetBool("isShooting", isShooting);
 
-            laser.GetComponent<Rigidbody2D>().velocity = fireJoystick.Direction.normalized * laserSpeed;
-            shootTimer = shootDelay + Time.time;
+        laserAngle = Mathf.Atan2(fireJoystick.Vertical, fireJoystick.Horizontal) * Mathf.Rad2Deg;
+        FlipBody(fireJoystick.Horizontal, playerBody);
+
+        bodyAngle = laserAngle + 90f;
+        StartCoroutine(RotateBody(bodyAngle));
+        returnToDefPosTimer = returnToDefPosTime + Time.time;
+
+    }
+
+    private bool GetIsShooting()
+    {
+        return fireJoystick.Direction != Vector2.zero;
+    }
+
+    private IEnumerator RotateBody(float targetAngle)
+    {
+        while (playerBody.transform.rotation.z != targetAngle)
+        {
+            playerBody.transform.rotation = Quaternion.Slerp(playerBody.transform.rotation, Quaternion.Euler(0f, 0f, targetAngle), turnSpeed * Time.deltaTime);
+            yield return null;
         }
+        yield return null;
+
+    }
+
+    private void FlipBody(float direction, GameObject gameObject)
+    {
+        if (direction < 0) {
+            gameObject.transform.localScale = flipScale;
+        }
+        else
+        {
+            gameObject.transform.localScale = regularScale;
+        }
+    }
+
+    public void InstantiateLaser()
+    {
+        GameObject laser = Instantiate(
+               laserPrefab,
+               gun.transform.position,
+               Quaternion.Euler(new Vector3(0, 0, laserAngle)));
+
+        laser.GetComponent<Rigidbody2D>().velocity = fireJoystick.Direction.normalized * laserSpeed;
+    }
+
+    public void ReturnDefaultPosition()
+    {
+        StartCoroutine(RotateBody(90f));
+        playerBody.transform.localScale = regularScale;   
     }
 }   
